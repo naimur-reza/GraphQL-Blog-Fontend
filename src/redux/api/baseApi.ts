2735a503fb94876bdedc8e041e726d2b4f635355
Naimur Reza <naimur.rezaa@gmail.com>
2024-02-17 18:12:16 +0600

    handle jwt status

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,20 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logout } from "../features/auth/authSlice";
+
+// https://product-management-naimur-reza.vercel.app/api/v1
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://product-management-naimur-reza.vercel.app/api/v1/",
+  baseUrl: "http://localhost:5000/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
@@ -11,9 +23,23 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithoutToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOption): Promise<any> => {
+  const result = await baseQuery(args, api, extraOption);
+
+  if (result.error?.status === 401) {
+    api.dispatch(logout());
+  }
+
+  return result;
+};
+
 const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery,
+  baseQuery: baseQueryWithoutToken,
   endpoints: () => ({}),
   tagTypes: ["Product", "Auth"],
 });
